58bb3c1fb85b42787492759e4fd9fe12326647ab
Sarvesh <sarvesh1challawar@gmail.com>
2020-07-08 01:04:04 +0530

    Dark Mode Added

--- scripts.js
+++ scripts.js
@@ -17,4 +17,24 @@ $(document).ready(function(){
 		var input=$('#pass');
 		input.attr('type')==='password'?input.attr('type','text'):input.attr('type','password');
 	});
+	
+	// new Darkmode().showWidget();
+
+	//Code from official site of Darkmode.js
+	var options = {
+		  bottom: '64px', // default: '32px'
+		  right: 'unset', // default: '32px'
+		  left: '32px', // default: 'unset'
+		  time: '0.5s', // default: '0.3s'
+		  mixColor: '#fff', // default: '#fff'
+		  backgroundColor: '#ffffff',  // default: '#fff'
+		  buttonColorDark: '#100f2c',  // default: '#100f2c'
+		  buttonColorLight: '#fff', // default: '#fff'
+		  saveInCookies: false, // default: true,
+		  label: '🌓', // default: ''
+		  autoMatchOsTheme: true // default: true
+		}
+
+	const darkmode = new Darkmode(options);
+	darkmode.showWidget();
 });
